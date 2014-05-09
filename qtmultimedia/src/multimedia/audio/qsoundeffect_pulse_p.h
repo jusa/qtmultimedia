cf4586ffdfc7f3985512cda8583c83dbf515341a
Juho Hämäläinen <juho.hamalainen@tieto.com>
2014-05-09 18:36:26 +0300

    [qsoundeffect] Set volume only if volume was explicitly set.

--- qtmultimedia/src/multimedia/audio/qsoundeffect_pulse_p.h
+++ qtmultimedia/src/multimedia/audio/qsoundeffect_pulse_p.h
@@ -181,6 +181,10 @@ private:
     ResourceStatus m_resourceStatus;
 
     QMediaPlayerResourceSetInterface *m_resources;
+
+#if defined(Q_WS_MAEMO_6) || defined(NEMO_AUDIO)
+    bool m_customVolume;
+#endif
 };
 
 QT_END_NAMESPACE
